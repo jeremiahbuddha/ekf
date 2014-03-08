dddfdb5607fc62eb23541aba25d01f929430caa4
jonathonsmith <jonathon.j.smith@jpl.nasa.gov>
2014-03-07 18:56:17 -0800

    Split AgentGroup into own files.

---
+++ AgentGroup.cpp
@@ -0,0 +1,12 @@
+
+#include <AgentGroup.hpp>
+
+AgentGroup::
+AgentGroup()
+{
+}
+
+AgentGroup::                                                                     
+~AgentGroup()                                                                     
+{                                                                                
+}    
---
+++ AgentGroup.hpp
@@ -0,0 +1,16 @@
+#ifndef EKF_AGENTGROUP_INCLUDE_                                                      
+#define EKF_AGENTGROUP_INCLUDE_ 
+
+class AgentGroup 
+{
+   /* 
+   An Agent is any named parameter in the universe which has both a quantity 
+   and a relationship with other Agents. 
+   */
+   public:
+      AgentGroup();
+      ~AgentGroup();
+   private:
+};
+
+#endif // Include guard
